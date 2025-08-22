61d5a70e261d27aa8b57d0429589ad67e9a83664
Alexander Brandon Coles <a.coles@openproject.com>
2025-08-22 19:58:41 +0100

    Make MyPageComponent use default encapsulation

--- frontend/src/app/features/my-page/my-page.component.ts
+++ frontend/src/app/features/my-page/my-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { GRID_PROVIDERS } from 'core-app/shared/components/grids/grid/grid.component';
 import { GridPageComponent } from 'core-app/shared/components/grids/grid/page/grid-page.component';
 
@@ -6,7 +6,6 @@ import { GridPageComponent } from 'core-app/shared/components/grids/grid/page/gr
   templateUrl: '../../shared/components/grids/grid/page/grid-page.component.html',
   styleUrls: ['../../shared/components/grids/grid/page/grid-page.component.sass'],
   providers: GRID_PROVIDERS,
-  encapsulation: ViewEncapsulation.None,
   standalone: false,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
