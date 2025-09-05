f6a629faec057387768cce993ecf429f18ac2400
Bruno Pagno <b.pagno@openproject.com>
2025-09-05 16:28:46 +0200

    use rails_helper instead of spec_helper
    Co-authored-by: Alexander Brandon Coles <a.coles@openproject.com>

--- modules/documents/spec/features/block_note_editor_spec.rb
+++ modules/documents/spec/features/block_note_editor_spec.rb
@@ -28,7 +28,7 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-require "spec_helper"
+require "rails_helper"
 
 RSpec.describe "BlockNote editor rendering", :js do
   let(:admin) { create(:admin) }
