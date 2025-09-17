125f1b9d12aeee44b58af82227da9b22b038b95b
Cyril Rohr <hey@cyrilrohr.com>
2025-09-17 16:20:22 +0200

    Dynamically generate the matrix

--- script/gh/docker-tags.rb
+++ script/gh/docker-tags.rb
@@ -5,6 +5,36 @@ require "optparse"
 require "net/http"
 require "json"
 
+def latest_release_major_minor
+  uri = URI("https://api.github.com/repos/opf/openproject/releases/latest")
+  res = Net::HTTP.get_response(uri)
+  if res.is_a?(Net::HTTPSuccess)
+    tag_name = JSON.parse(res.body)["tag_name"]
+    tag_name[/^v?(\d+\.\d+)/, 1]
+  end
+rescue StandardError
+  puts "Error getting latest release major: #{$!}"
+  nil
+end
+
+def generate_matrix
+  latest_version = latest_release_major_minor
+  return nil unless latest_version
+
+  {
+    "include" => [
+      {
+        "branch" => "dev",
+        "tag" => "dev"
+      },
+      {
+        "branch" => "release/#{latest_version}",
+        "tag" => "#{latest_version}-rc"
+      }
+    ]
+  }
+end
+
 class Tag
   def initialize(tag)
     @tag = tag
@@ -22,30 +52,18 @@ class Tag
     @tag.sub(/^v/, "").sub(/-rc$/, "")
   end
 
-  def latest_release_major_minor
-    uri = URI("https://api.github.com/repos/opf/openproject/releases/latest")
-    res = Net::HTTP.get_response(uri)
-    if res.is_a?(Net::HTTPSuccess)
-      tag_name = JSON.parse(res.body)["tag_name"]
-      tag_name[/^v?(\d+\.\d+)/, 1]
-    end
-  rescue
-    puts "Error getting latest release major: #{$!}"
-    nil
-  end
-
   def to_semver_docker_tags
     if semver?
       [
         "type=semver,pattern={{version}},value=#{version}",
         "type=semver,pattern={{major}}.{{minor}},value=#{version}",
-        "type=semver,pattern={{major}},value=#{version}",
+        "type=semver,pattern={{major}},value=#{version}"
       ]
     elsif rc?
       latest_major_minor = latest_release_major_minor
       tags = [
         "type=raw,value=#{major}.#{minor}-rc",
-        "type=raw,value=#{major}-rc",
+        "type=raw,value=#{major}-rc"
       ]
       if latest_major_minor && latest_major_minor == [major, minor].join(".")
         tags << [
@@ -57,7 +75,7 @@ class Tag
       ["type=raw,value=#{version}"]
     end
   end
-  
+
   def major
     return unless semver? || rc?
 
@@ -71,9 +89,7 @@ class Tag
   end
 end
 
-
-# rubocop:disable Metrics/AbcSize
-def main
+def main # rubocop:disable Metrics/AbcSize
   options = {}
   OptionParser.new do |opts|
     opts.banner = "Usage: #{$0} [TAG] [options]"
@@ -83,20 +99,33 @@ def main
     opts.on("--version", "Output first tag as version") do
       options[:version] = true
     end
+    opts.on("--matrix", "Generate matrix for GitHub Actions workflow") do
+      options[:matrix] = true
+    end
     opts.on("-h", "--help", "Prints this help") do
       puts opts
       exit
     end
   end.parse!
 
-  tag = Tag.new(ARGV.first)
-  if options[:version]
-    puts tag.version
-  elsif options[:format_for_docker]
-    puts tag.to_semver_docker_tags.join("\n")
+  if options[:matrix]
+    matrix = generate_matrix
+    if matrix
+      puts JSON.pretty_generate(matrix)
+    else
+      puts "Error: Could not generate matrix"
+      exit 1
+    end
   else
-    puts "Error: Must specify either --version or --format-for-docker"
-    exit 1
+    tag = Tag.new(ARGV.first)
+    if options[:version]
+      puts tag.version
+    elsif options[:format_for_docker]
+      puts tag.to_semver_docker_tags.join("\n")
+    else
+      puts "Error: Must specify either --version, --format-for-docker, or --matrix"
+      exit 1
+    end
   end
 end
 
