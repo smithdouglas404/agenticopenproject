7a93e5a5f0f165255fdb612a03d4be1f397a2871
ulferts <jens.ulferts@googlemail.com>
2025-10-10 09:50:33 +0200

    spec baseline with and without allowed_to perf improvement

--- spec/requests/api/v3/work_packages/index_resource_spec.rb
+++ spec/requests/api/v3/work_packages/index_resource_spec.rb
@@ -244,7 +244,7 @@ RSpec.describe "API v3 Work package resource",
       end
     end
 
-    context "when providing timestamps", with_ee: %i[baseline_comparison] do
+    shared_examples_for "when providing timestamps" do
       subject do
         get path
         last_response
@@ -1344,6 +1344,16 @@ RSpec.describe "API v3 Work package resource",
             end
           end
         end
+      end
+    end
+
+    describe "baseline comparison", with_ee: %i[baseline_comparison] do
+      context "without large_instance_wp_allowed_to_sql active", with_settings: { large_instance_wp_allowed_to_sql: false } do
+        it_behaves_like "when providing timestamps"
+      end
+
+      context "with large_instance_wp_allowed_to_sql active", with_settings: { large_instance_wp_allowed_to_sql: true } do
+        it_behaves_like "when providing timestamps"
       end
     end
   end
