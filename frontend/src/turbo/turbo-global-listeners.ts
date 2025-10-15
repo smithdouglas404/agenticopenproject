f4d0b4c3c1d1429dee2301ec6fb90907f54c07be
Alexander Brandon Coles <a.coles@openproject.com>
2025-10-15 16:01:27 +0100

    Remove legacy toggleable fieldset behavior, styles

--- frontend/src/turbo/turbo-global-listeners.ts
+++ frontend/src/turbo/turbo-global-listeners.ts
@@ -1,7 +1,6 @@
 import { DeviceService } from 'core-app/core/browser/device.service';
 import { scrollHeaderOnMobile } from 'core-app/core/setup/globals/global-listeners/top-menu-scroll';
 import { detectOnboardingTour } from 'core-app/core/setup/globals/onboarding/onboarding_tour_trigger';
-import { setupToggableFieldsets } from 'core-app/core/setup/globals/global-listeners/toggable-fieldset';
 import { installMenuLogic } from 'core-app/core/setup/globals/global-listeners/action-menu';
 import { listenToSettingChanges } from 'core-app/core/setup/globals/global-listeners/settings';
 import { makeColorPreviews } from 'core-app/core/setup/globals/global-listeners/color-preview';
@@ -37,9 +36,6 @@ export function addTurboGlobalListeners() {
     //
     // Legacy scripts from app/assets that are not yet component based
     //
-
-    // Toggable fieldsets
-    setupToggableFieldsets();
 
     // Action menu logic
     jQuery('.toolbar-items').each((_, menu:HTMLElement) => {
