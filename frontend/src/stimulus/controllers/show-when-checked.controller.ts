4f697ffdc637245201f4d13b831a35d10117c138
Alexander Brandon Coles <a.coles@openproject.com>
2025-10-29 19:34:29 +0000

    Make show-when-checked apply aria-hidden
    Only applied when using CSS classes to toggle visibility.
    Also extracts visibility function into shared helpers file.

--- frontend/src/stimulus/controllers/show-when-checked.controller.ts
+++ frontend/src/stimulus/controllers/show-when-checked.controller.ts
@@ -28,6 +28,7 @@
  * ++
  */
 
+import { toggleElement, toggleElementByClass, toggleElementByVisibility } from 'core-app/shared/helpers/dom-helpers';
 import { ApplicationController } from 'stimulus-use';
 
 // can show OR hide items by default (with "reversed" set to hide on checking)
@@ -77,11 +78,11 @@ export default class OpShowWhenCheckedController extends ApplicationController {
         }
 
         if (el.dataset.setVisibility === 'true') {
-          el.style.setProperty('visibility', shouldShow ? 'visible' : 'hidden');
-        } else if (el.dataset.visibilityClass) {
-          el.classList.toggle(el.dataset.visibilityClass, !shouldShow);
+          toggleElementByVisibility(el, shouldShow);
+        } else if ('visibilityClass' in el.dataset) {
+          toggleElementByClass(el, el.dataset.visibilityClass!, shouldShow);
         } else {
-          el.hidden = !shouldShow;
+          toggleElement(el, shouldShow);
         }
       });
   }
