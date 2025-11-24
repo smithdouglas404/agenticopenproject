0d8053395585c3030e08f14c19d8fb7689ce0215
Klaus Zanders <k.zanders@openproject.com>
2025-11-24 14:38:07 +0100

    Aggregate migration `20241129135602_populate_manage_own_reminders_permission`

--- db/migrate/1000016_aggregated_migrations.rb
+++ db/migrate/1000016_aggregated_migrations.rb
@@ -412,6 +412,7 @@ class AggregatedMigrations < SquashedMigration
     20241125161226_unique_index_on_project_life_cycle_steps
     20241126111225_add_project_life_cycle_step_roles
     20241127161228_grant_select_project_life_cycle_permission
+    20241129135602_populate_manage_own_reminders_permission
   ]
   # rubocop:enable Metrics/CollectionLiteralLength
 end
