d7292ae0c242299411aec2a35d95006dd3438e95
Jan Sandbrink <j.sandbrink@openproject.com>
2025-12-01 16:35:22 +0100

    First implementation of MCP API server in OpenProject
    So far the MCP server only offers a single tool, but authentication
    and integration is already built in a way that's intended to last.
    
    Ideally extensions to this happen by adding additional tools or resources,
    but will not require further architectural changes, though realistically we'll
    probably identify more potential for reuse, once we added a few more tools.
    
    The exact representation of results is still slightly to-be-discussed. Right
    now we are using vanilla APIv3 representation, which might be enough, but possibly
    we want to represent linked resources differently, so that they can be recognized
    to be fetchable via MCP resources more easily.

--- Gemfile
+++ Gemfile
@@ -159,6 +159,8 @@ gem "ttfunk", "~> 1.7.0" # remove after https://github.com/prawnpdf/prawn/issues
 # prawn implicitly depends on matrix gem no longer in ruby core with 3.1
 gem "matrix", "~> 0.4.3"
 
+gem "mcp", "~> 0.4.0"
+
 gem "meta-tags", "~> 2.22.2"
 
 gem "paper_trail", "~> 17.0.0"
@@ -198,7 +200,7 @@ gem "aws-sdk-core", "~> 3.239"
 # File upload via fog + screenshots on travis
 gem "aws-sdk-s3", "~> 1.206"
 
-gem "openproject-token", "~> 8.3.0"
+gem "openproject-token", "~> 8.4.0"
 
 gem "plaintext", "~> 0.3.7"
 
---
+++ app/services/mcp_tools/search_project.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module McpTools
+  class SearchProject < MCP::Tool
+    # TODO: The mcp gem does not support pagination, so we only limit the number of results for now
+    MAX_SIZE = 100
+    tool_name "search_project"
+    description "Search projects matching all of the passed input parameters. Parameters not passed are ignored. " \
+                "Results are limited to a maximum of #{MAX_SIZE} projects."
+
+    input_schema(
+      properties: {
+        name: { type: "string", description: "Name of the project. Accepts partial project names, not case-sensitive." },
+        identifier: { type: "string", description: "Project indentifier. Case-sensitive, matching exactly." },
+        status_code: { type: "string", enum: Project.status_codes.keys, description: "The project status." }
+      }
+    )
+
+    output_schema(
+      type: :array,
+      items: JsonSchemaLoader.new.load("project_model")
+    )
+
+    class << self
+      def call(name: nil, identifier: nil, status_code: nil)
+        query = { name:, identifier:, status_code: }.compact
+        result = if query.present?
+                   projects = projects_for_query(query)
+                   projects.map { |p| API::V3::Projects::ProjectRepresenter.create(p, current_user: User.current) }
+                 else
+                   []
+                 end
+
+        if Rails.env.development?
+          # We are only validating the output during development, so we can see errors during dev, but do not break the
+          # API in production due to minor schema differences.
+          output_schema.validate_result(result.to_json)
+        end
+
+        MCP::Tool::Response.new(
+          [{ type: "text", text: result.to_json }],
+          structured_content: result
+        )
+      end
+
+      private
+
+      def projects_for_query(query)
+        name = query.delete(:name)
+        projects = Project.visible.where(query).limit(MAX_SIZE)
+        projects = projects.where("name ILIKE '%#{OpenProject::SqlSanitization.quoted_sanitized_sql_like(name)}%'") if name
+        projects
+      end
+    end
+  end
+end
