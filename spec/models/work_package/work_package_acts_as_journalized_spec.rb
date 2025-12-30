0e80a5b4cd7b2387623d547f72837c3185e2d667
Christophe Bliard <c.bliard@openproject.com>
2025-12-30 14:43:41 +0100

    Fix test

--- spec/models/work_package/work_package_acts_as_journalized_spec.rb
+++ spec/models/work_package/work_package_acts_as_journalized_spec.rb
@@ -54,16 +54,16 @@ RSpec.describe WorkPackage do
 
     current_user { create(:user) }
 
-    # The below test currently fails with the following error:
+    # The below test was failing with the following error:
     # ERROR:  new row for relation "journals" violates check constraint "journals_validity_period_not_empty" (PG::CheckViolation)
     # DETAIL:  Failing row contains (1178, WorkPackage, 481, 1252, , 2025-12-04 07:58:21.028586+00, 1, 2025-12-04 07:58:21.028586+00, Journal::WorkPackageJournal, 833, {}, empty, f).
     it "can add multiple comments right after creation" do
       work_package
       User.execute_as current_user do
         work_package.add_journal(user: current_user, notes: "First comment")
-        work_package.save_journals # <== THIS WORKS
+        work_package.save_journals
         work_package.add_journal(user: current_user, notes: "Second comment")
-        work_package.save_journals # <== THIS FAILS
+        work_package.save_journals
       end
     end
 
