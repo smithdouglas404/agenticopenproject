opf/openproject

commit 5ea185ef66a4b3f556394e60afd39edfcc3604da

Placeholder user project members (#8961)
* remove intermediate placeholder scope

Doing so, placeholder users will begin to show up in the system

* remove scope without value

* extract scope

* use enum for status

* allow placeholder users to become project members

* display placeholder user member on members widget

* remove now superfluous method

The status name can simply be queried via #status now

* replace possible_assignees/responsibles on project

This also leads to placeholder users becoming eligible as assignees and
responsibles.

* fix aggregated scope on bulk edit

* linting

* remove IssueHelper

app/services/projects/copy/work_packages_dependent_service.rb
@@ -153,19 +153,20 @@ module Projects::Copy
     end
 
     def work_package_assigned_to_id(source_work_package)
-      assigned_to_id = source_work_package.assigned_to_id
-      return unless assigned_to_id
-
-      @assignees ||= target.possible_assignees.pluck(:id).to_set
-      assigned_to_id if @assignees.include?(assigned_to_id)
+      possible_principal_id(source_work_package.assigned_to_id,
+                            source_work_package.project)
     end
 
     def work_package_responsible_id(source_work_package)
-      responsible_id = source_work_package.responsible_id
-      return unless responsible_id
+      possible_principal_id(source_work_package.responsible_id,
+                            source_work_package.project)
+    end
 
-      @responsible ||= target.possible_responsibles.pluck(:id).to_set
-      responsible_id if @responsible.include?(responsible_id)
+    def possible_principal_id(principal_id, project)
+      return unless principal_id
+
+      @principals ||= Principal.possible_assignee(project).pluck(:id).to_set
+      principal_id if @principals.include?(principal_id)
     end
   end
 end

spec/services/projects/copy_service_integration_spec.rb
@@ -287,12 +287,13 @@ describe Projects::CopyService, 'integration', type: :model do
           query.add_filter('parent', '=', [source_wp.id.to_s])
           # Not valid due to wp not visible
           query.save!(validate: false)
+          query
         end
 
-        it 'produces a valid query that is mapepd in the new project' do
+        it 'produces a valid query that is mapped in the new project' do
           expect(subject).to be_success
           copied_wp = project_copy.work_packages.find_by(subject: 'source wp')
-          copied = project_copy.queries.first
+          copied = project_copy.queries.find_by(name: query.name)
           expect(copied.filters[1].values).to eq [copied_wp.id.to_s]
         end
       end

smithdouglas404/agenticopenproject

commit 8dd148e6409ce7a76dbdcb62a68c98424e7d3491

[70290] Project copy nilifies user fields if members are excluded
https://community.openproject.org/wp/70290

This fixes the regression introduced in 5ea185ef66a: the
`possible_principal_id` was run against the source project instead of
the target project, leading to always return something.

Fix is to always run `possible_principal_id` against the target project.

This was not detected by the unit tests: original tests were taking the
first work package of the copied project to check that the user field
was nil, assuming the first work package would be the copy of the one
having the user field set, but actually the work package was not copied,
so another work package is picked and this one has all user fields nil
because the original work package has all user fields nil.

The test was fixed by explicitly getting the copied work package using
the subject of the original work package, and failing if the copied work
package is not found.

app/services/projects/copy/work_packages_dependent_service.rb
@@ -149,13 +149,11 @@ module Projects::Copy
     end
 
     def work_package_assigned_to_id(source_work_package)
-      possible_principal_id(source_work_package.assigned_to_id,
-                            source_work_package.project)
+      possible_principal_id(source_work_package.assigned_to_id)
     end
 
     def work_package_responsible_id(source_work_package)
-      possible_principal_id(source_work_package.responsible_id,
-                            source_work_package.project)
+      possible_principal_id(source_work_package.responsible_id)
     end
 
     def custom_value_attributes(source_work_package, user_cf_ids)
@@ -168,10 +166,10 @@ module Projects::Copy
       end
     end
 
-    def possible_principal_id(principal_id, project)
+    def possible_principal_id(principal_id)
       return unless principal_id
 
-      @principals ||= Principal.possible_assignee(project).pluck(:id).to_set
+      @principals ||= Principal.possible_assignee(target).pluck(:id).to_set
       principal_id if @principals.include?(principal_id)
     end
 

spec/services/projects/copy_service_integration_spec.rb
@@ -130,6 +130,14 @@ RSpec.describe(
     let(:all_modules) { described_class.copyable_dependencies.pluck(:identifier) }
     let(:project_copy) { subject.result }
 
+    def copy_of(work_package)
+      copied_work_package = project_copy.work_packages.find_by(subject: work_package.subject)
+      expect(copied_work_package).not_to be_nil,
+                                         "Expected work package '#{work_package.subject}' to be copied to " \
+                                         "project '#{project_copy.name}' but was not"
+      copied_work_package
+    end
+
     shared_examples_for "copies public attribute" do
       describe "#public" do
         before do
@@ -969,8 +977,7 @@ RSpec.describe(
 
             it "copies the assigned_to" do
               expect(subject).to be_success
-              expect(project_copy.work_packages[0].assigned_to)
-                .to eql assigned_user
+              expect(copy_of(work_package).assigned_to).to eq(assigned_user)
               # The assignee of the new work package receives a notification
               expect { perform_enqueued_jobs }
                 .to change(Notification.where(recipient: assigned_user), :count)
@@ -984,8 +991,7 @@ RSpec.describe(
 
             it "nils the assigned_to" do
               expect(subject).to be_success
-              expect(project_copy.work_packages[0].assigned_to)
-                .to be_nil
+              expect(copy_of(work_package).assigned_to).to be_nil
               # No notification is sent out
               expect { perform_enqueued_jobs }
                 .not_to change(Notification.where(recipient: assigned_user), :count)
@@ -1008,8 +1014,7 @@ RSpec.describe(
 
             it "copies the responsible" do
               expect(subject).to be_success
-              expect(project_copy.work_packages[0].responsible)
-                .to eql responsible_user
+              expect(copy_of(work_package).responsible).to eq(responsible_user)
               # The responsible of the new work package receives a notification
               expect { perform_enqueued_jobs }
                 .to change(Notification.where(recipient: responsible_user), :count)
@@ -1021,9 +1026,9 @@ RSpec.describe(
           context "with the member being not copied" do
             let(:only_args) { %w[work_packages] }
 
-            it "nils the assigned_to" do
+            it "nils the responsible" do
               expect(subject).to be_success
-              expect(project_copy.work_packages[0].responsible).to be_nil
+              expect(copy_of(work_package).responsible).to be_nil
               # No notification is sent out
               expect { perform_enqueued_jobs }
                 .not_to change(Notification.where(recipient: responsible_user), :count)
@@ -1052,8 +1057,7 @@ RSpec.describe(
 
             it "copies the custom_field" do
               expect(subject).to be_success
-              wp = project_copy.work_packages.find_by(subject: work_package.subject)
-              expect(wp.send(custom_field.attribute_getter)).to eql other_user
+              expect(copy_of(work_package).send(custom_field.attribute_getter)).to eql other_user
             end
           end
 
@@ -1062,8 +1066,7 @@ RSpec.describe(
 
             it "nils the custom_field" do
               expect(subject).to be_success
-              wp = project_copy.work_packages.find_by(subject: work_package.subject)
-              expect(wp.send(custom_field.attribute_getter)).to be_nil
+              expect(copy_of(work_package).send(custom_field.attribute_getter)).to be_nil
             end
           end
         end