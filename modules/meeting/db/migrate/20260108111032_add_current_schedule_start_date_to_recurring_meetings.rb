387a2ed76580516df61ee9a7adc1dabf808142e3
Klaus Zanders <k.zanders@openproject.com>
2026-01-09 12:03:25 +0100

    Store time for schedule start, not date

--- modules/meeting/db/migrate/20260108111032_add_current_schedule_start_date_to_recurring_meetings.rb
+++ modules/meeting/db/migrate/20260108111032_add_current_schedule_start_date_to_recurring_meetings.rb
@@ -2,12 +2,14 @@
 
 class AddCurrentScheduleStartDateToRecurringMeetings < ActiveRecord::Migration[8.0]
   def change
-    add_column :recurring_meetings, :current_schedule_start_date, :date
+    add_column :recurring_meetings, :current_schedule_start, :datetime
 
-    execute <<~SQL.squish
-      UPDATE recurring_meetings SET current_schedule_start_date = (start_time AT TIME ZONE time_zone)::date;
-    SQL
+    up_only do
+      execute <<~SQL.squish
+        UPDATE recurring_meetings SET current_schedule_start = (start_time AT TIME ZONE time_zone);
+      SQL
 
-    change_column_null :recurring_meetings, :current_schedule_start_date, false
+      change_column_null :recurring_meetings, :current_schedule_start, false
+    end
   end
 end
