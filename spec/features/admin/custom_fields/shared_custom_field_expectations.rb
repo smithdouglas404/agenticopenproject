c0e671f68e18361008e0d26a24e5448990244629
Ivan Kuchin <i.kuchin@openproject.com>
2026-01-12 21:01:49 +0100

    expect is for all field for work package and project custom field

--- spec/features/admin/custom_fields/shared_custom_field_expectations.rb
+++ spec/features/admin/custom_fields/shared_custom_field_expectations.rb
@@ -147,6 +147,7 @@ RSpec.shared_examples_for "expected fields for the custom field's format", :aggr
   # Form element labels, default English translation in the trailing comment:
   let(:label_name) { I18n.t("attributes.name") } # Name
   let(:label_section) { I18n.t("activerecord.attributes.project_custom_field.custom_field_section") } # Section
+  let(:label_is_for_all) { I18n.t("attributes.is_for_all") } # For all projects
   let(:label_min_length) { I18n.t("activerecord.attributes.custom_field.min_length") } # Minimum length
   let(:label_max_length) { I18n.t("activerecord.attributes.custom_field.max_length") } # Maximum length
   let(:label_regexp) { I18n.t("activerecord.attributes.custom_field.regexp") } # Regular expression
@@ -176,6 +177,12 @@ RSpec.shared_examples_for "expected fields for the custom field's format", :aggr
       expect(page).to have_field(label_section)
     else
       expect(page).to have_no_label(label_section)
+    end
+
+    if type in "Work package" | "Project"
+      expect(page).to have_field(label_is_for_all)
+    else
+      expect(page).to have_no_label(label_is_for_all)
     end
 
     if format in "Text" | "Integer" | "Float" | "Long text"
