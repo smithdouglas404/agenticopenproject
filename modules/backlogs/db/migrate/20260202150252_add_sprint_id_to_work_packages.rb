d4ec81c3d5bb119095a5f66796a656740f72a5c1
Tobias Dillmann <github@pentokami.de>
2026-02-02 16:19:26 +0100

    [#71248] Connect Sprints to WorkPackages

--- modules/backlogs/db/migrate/20260202150252_add_sprint_id_to_work_packages.rb
+++ modules/backlogs/db/migrate/20260202150252_add_sprint_id_to_work_packages.rb
@@ -28,39 +28,10 @@
 # See COPYRIGHT and LICENSE files for more details.
 #++
 
-# Intended to eventually replace the `Sprint` model from models/sprint.rb
-# Namespaced for now so that the rest of the application can keep using the old model.
-# Remove this namespace and the old class once all usages have been replaced.
-module Agile
-  class Sprint < ApplicationRecord
-    self.table_name = "sprints"
+class AddSprintIdToWorkPackages < ActiveRecord::Migration[8.0]
+  def change
+    add_column :work_packages, :sprint_id, :integer, default: nil, null: true
 
-    has_many :work_packages, dependent: :nullify
-    has_many :projects, through: :work_packages
-
-    enum :status, {
-      "in planning" => "in_planning",
-      "active" => "active",
-      "completed" => "completed"
-    }, default: "in_planning"
-
-    validates :name, presence: true
-    validates :status, presence: true, inclusion: { in: statuses.keys }
-    validates :start_date, presence: true
-    validates :end_date, presence: true
-
-    validate :end_date_after_start_date
-
-    # TODO: sharing, work package journal
-
-    private
-
-    def end_date_after_start_date
-      return if end_date.blank? || start_date.blank?
-
-      if end_date < start_date
-        errors.add(:end_date, "must be after the start date")
-      end
-    end
+    add_index :work_packages, :sprint_id
   end
 end
