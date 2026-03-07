fdcf93e2cf6092f98f269fa53d986b7874e34c21
Tobias Dillmann <github@pentokami.de>
2026-03-07 13:26:08 +0100

    [#71250] Collect all sprints for the current project
    Will not change anything now, but will be relevant with sharing
    implemented.

--- modules/backlogs/app/controllers/rb_master_backlogs_controller.rb
+++ modules/backlogs/app/controllers/rb_master_backlogs_controller.rb
@@ -70,7 +70,7 @@ class RbMasterBacklogsController < RbApplicationController
     @owner_backlogs = Backlog.owner_backlogs(@project)
 
     if OpenProject::FeatureDecisions.scrum_projects_active?
-      @sprints = @project.sprints.not_completed.order_by_date
+      @sprints = Agile::Sprint.for_project(@project).not_completed.order_by_date
     else
       @sprint_backlogs = Backlog.sprint_backlogs(@project)
     end
