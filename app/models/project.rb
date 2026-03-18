bab168081a74f1d738ee9443eb04883e345eba22
Tomas Hykel <mail@thykel.cz>
2026-03-18 23:44:51 +0100

    simplify the special character validation rule

--- app/models/project.rb
+++ app/models/project.rb
@@ -221,12 +221,9 @@ class Project < ApplicationRecord
             if: ->(p) { p.identifier_changed? && p.identifier.present? && Setting::WorkPackageIdentifier.alphanumeric? }
 
   validates :identifier,
-            format: { with: /\A[A-Z][A-Z0-9_]*\z/, message: :no_special_characters },
+            format: { with: /[A-Z][A-Z0-9_]*\z/, message: :no_special_characters },
             length: { maximum: SEMANTIC_IDENTIFIER_MAX_LENGTH },
-            if: ->(p) {
-              p.identifier_changed? && p.identifier.present? && Setting::WorkPackageIdentifier.alphanumeric? &&
-                          p.identifier.match?(/\A[A-Z]/)
-            }
+            if: ->(p) { p.identifier_changed? && p.identifier.present? && Setting::WorkPackageIdentifier.alphanumeric? }
 
   validates_associated :repository, :wiki
 
