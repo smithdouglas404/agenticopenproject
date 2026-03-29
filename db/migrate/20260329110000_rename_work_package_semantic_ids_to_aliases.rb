47d697214d306fd964d07eb257ff94880d34b992
Tomas Hykel <mail@thykel.cz>
2026-03-29 18:12:03 +0200

    remove superfluous migrations

--- db/migrate/20260329110000_rename_work_package_semantic_ids_to_aliases.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RenameWorkPackageSemanticIdsToAliases < ActiveRecord::Migration[8.1]
-  def change
-    rename_table :work_package_semantic_ids, :work_package_semantic_aliases
-  end
-end
