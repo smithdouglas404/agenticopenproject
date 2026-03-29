f3fc8a7399dcd03fb2e6505a729725155b419493
Tomas Hykel <mail@thykel.cz>
2026-03-29 18:42:44 +0200

    wip

--- app/workers/work_packages/identifier_autofix/apply_handles_job.rb
+++ app/workers/work_packages/identifier_autofix/apply_handles_job.rb
@@ -29,12 +29,12 @@
 #++
 
 class WorkPackages::IdentifierAutofix::ApplyHandlesJob < ApplicationJob
-  # FIXME: The admin UI's job_in_progress? query and :change_in_progress state
-  # assume at most one active instance of this job at any given time.
-  # Enforce this with good_job_control_concurrency_with(perform_limit: 1)
-  # when the real migration body is implemented.
+  include GoodJob::ActiveJobExtensions::Concurrency
+
+  good_job_control_concurrency_with(perform_limit: 1)
+
   def perform
-    # FIXME: replace with actual project handle migration
-    sleep 5
+    # TODO: convert the project identifiers
+    WorkPackages::SemanticIds::BackfillService.run
   end
 end
