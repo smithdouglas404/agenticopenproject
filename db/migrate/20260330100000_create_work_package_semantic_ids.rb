6dc7474ece4467e773a939df7d53ef236ba3d758
Tomas Hykel <mail@thykel.cz>
2026-03-30 22:48:47 +0200

    clean up migration

--- db/migrate/20260330100000_create_work_package_semantic_ids.rb
+++ db/migrate/20260330100000_create_work_package_semantic_ids.rb
@@ -58,10 +58,6 @@ class CreateWorkPackageSemanticIds < ActiveRecord::Migration[8.1]
               unique: true,
               where: "sequence_number IS NOT NULL",
               if_not_exists: true
-
-    # Remove legacy current flag if the table was created by an older migration
-    remove_index :work_package_semantic_aliases, name: :idx_wp_semantic_ids_current, if_exists: true
-    remove_column :work_package_semantic_aliases, :current, :boolean, if_exists: true
   end
 
   def down
