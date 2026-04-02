6874274a62185718b98dd8a2edafee054b68667d
shanjian <shanjianli@gmail.com>
2026-04-02 17:34:36 +0000

    Stabilize board feature test interactions
    Use the NewBoard page helper for opening board creation in enterprise board feature specs and harden helper waits for cuprite/async behavior.\n\nAlso tighten work package status field editing helper activation timing to reduce flaky interactions in feature tests.

--- modules/boards/spec/features/support/board_new_page.rb
+++ modules/boards/spec/features/support/board_new_page.rb
@@ -8,12 +8,22 @@ module Pages
 
     def visit!
       visit new_work_package_board_path
+      wait_for_reload if using_cuprite?
+      wait_for_network_idle if using_cuprite?
     end
 
-    def navigate_by_create_button
-      visit work_package_boards_path unless page.current_path == work_package_boards_path
+    def navigate_by_create_button(path: work_package_boards_path)
+      visit path unless page.current_path == path
+      wait_for_network_idle if using_cuprite?
 
-      page.find_test_selector("add-board-button").click
+      button = page.first(:test_id,
+                          "add-board-button",
+                          text: I18n.t("boards.label_board"),
+                          exact_text: true,
+                          visible: :visible) || page.find(:test_id, "add-board-button", visible: :visible)
+
+      button.click
+      wait_for_reload if using_cuprite?
     end
 
     def set_title(title)
@@ -27,8 +37,7 @@ module Pages
     def set_project(project)
       select_autocomplete(find('[data-test-selector="project_id"]'),
                           query: project,
-                          results_selector: "body",
-                          wait_for_fetched_options: false)
+                          results_selector: "body")
     end
 
     def set_board_type(board_type)
