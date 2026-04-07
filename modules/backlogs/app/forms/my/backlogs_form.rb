5ef00d87bf68f93afa6fab1e2aaa68d608a1bf60
Henriette Darge <h.darge@openproject.com>
2026-04-07 09:12:29 +0200

    Remove empty lines

--- modules/backlogs/app/forms/my/backlogs_form.rb
+++ modules/backlogs/app/forms/my/backlogs_form.rb
@@ -31,14 +31,12 @@
 class My::BacklogsForm < ApplicationForm
   form do |f|
     f.fieldset_group(title: helpers.t("backlogs.user_preference.header_backlogs"), mt: 4) do |fg|
-
       unless OpenProject::FeatureDecisions.scrum_projects_active?
         fg.text_field name: :task_color,
                       label: helpers.t("backlogs.task_color"),
                       value: @color,
                       input_width: :xsmall
       end
-
 
       fg.check_box name: :versions_default_fold_state,
                    value: DEFAULT_FOLD_STATE,
