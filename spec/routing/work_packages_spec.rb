8802ce5871e524b9070d2dd48afbce9225e34ce2
Kabiru Mwenja <k.mwenja@openproject.com>
2026-04-10 20:06:28 +0300

    Add routing spec for semantic work package identifiers
    Verifies that GET /work_packages/PROJ-42 routes to work_packages#show,
    matching the widened route constraint.

--- spec/routing/work_packages_spec.rb
+++ spec/routing/work_packages_spec.rb
@@ -92,6 +92,12 @@ RSpec.describe WorkPackagesController do
                                                 id: "1")
   end
 
+  it "connects GET /work_packages/:id to work_packages#show with semantic identifier" do
+    expect(get("/work_packages/PROJ-42")).to route_to(controller: "work_packages",
+                                                      action: "show",
+                                                      id: "PROJ-42")
+  end
+
   it "connects GET /work_packages/:id/share to work_packages/shares#index" do
     expect(get("/work_packages/1/shares")).to route_to(controller: "shares",
                                                        action: "index",
