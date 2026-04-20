24cd94f0b0a7331bddf6aeefa7a4858b0d4a0a6e
Ivan Kuchin <i.kuchin@openproject.com>
2026-04-20 19:45:26 +0200

    load backlogs only when displaying them using turbo frame

--- modules/backlogs/app/controllers/backlogs/backlog_controller.rb
+++ modules/backlogs/app/controllers/backlogs/backlog_controller.rb
@@ -36,11 +36,11 @@ module Backlogs
       :backlog
     end
 
-    before_action :load_backlogs, only: :show
-
     def show
       case turbo_frame_request_id
       when "backlogs_container"
+        load_backlogs
+
         render partial: "backlogs/backlog/backlog_list", layout: false
       else
         render "backlogs/backlog/show"
@@ -52,6 +52,7 @@ module Backlogs
         render "work_packages/split_view", layout: false
       else
         load_backlogs
+
         render "backlogs/backlog/show"
       end
     end
