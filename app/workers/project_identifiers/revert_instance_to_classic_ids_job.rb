586658557f9497f8f8c909c03feae2c8b80992d3
Tomas Hykel <mail@thykel.cz>
2026-04-24 02:27:59 +0200

    10 attempts should be fine

--- app/workers/project_identifiers/revert_instance_to_classic_ids_job.rb
+++ app/workers/project_identifiers/revert_instance_to_classic_ids_job.rb
@@ -43,7 +43,7 @@ class ProjectIdentifiers::RevertInstanceToClassicIdsJob < ApplicationJob
 
   good_job_control_concurrency_with(total_limit: 1)
   retry_on StandardError, wait: :polynomially_longer, attempts: 8
-  retry_on IdConflictsRemain, wait: 0, attempts: 30
+  retry_on IdConflictsRemain, wait: 0, attempts: 10
 
   def perform
     raise "expected Setting.work_packages_identifier to be classic" unless Setting::WorkPackageIdentifier.classic?
