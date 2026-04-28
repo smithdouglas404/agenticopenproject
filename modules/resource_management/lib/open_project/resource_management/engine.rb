ecc0b0972b270cf340c06926717cc706f8b7b4fe
Klaus Zanders <k.zanders@openproject.com>
2026-04-28 12:04:13 +0200

    Resource Management Engine Boilerplate

---
+++ modules/resource_management/lib/open_project/resource_management/engine.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+require "open_project/plugins"
+
+module OpenProject::ResourceManagement
+  class Engine < ::Rails::Engine
+    engine_name :openproject_resource_management
+
+    include OpenProject::Plugins::ActsAsOpEngine
+
+    initializer "openproject-resource_management.feature_decisions" do
+      OpenProject::FeatureDecisions.add :resource_management
+    end
+
+    register "openproject-resource_management",
+             author_url: "https://www.openproject.org",
+             bundled: true,
+             settings: {} do
+      project_module :resource_management do
+        permission :view_resource_planners,
+                   { "resource_management/resource_planners": %i[index show overview] },
+                   permissible_on: :project
+
+        permission :manage_public_resource_planners,
+                   { "resource_management/resource_planners": %i[index show new create edit update destroy] },
+                   permissible_on: :project,
+                   dependencies: %i[view_resource_planners]
+      end
+
+      # TODO: Add those menus when global overview will be implemented
+      #    should_render_global_menu_item = Proc.new do
+      #      (User.current.logged? || !Setting.login_required?) &&
+      #        User.current.allowed_in_any_project?(:view_resources) &&
+      #        OpenProject::FeatureDecisions.resource_management_active?
+      #    end
+
+      #    menu :global_menu,
+      #         :resource_management,
+      #         { controller: "/resource_management/resource_management", action: :overview },
+      #         caption: :label_resource_management,
+      #         after: :calendar_view,
+      #         icon: "people",
+      #         if: should_render_global_menu_item
+
+      #    menu :top_menu,
+      #         :resource_management,
+      #         { controller: "/resource_management/resource_management", action: :overview },
+      #         context: :modules,
+      #         caption: :label_resource_management,
+      #         after: :calendar_view,
+      #         icon: "people",
+      #         if: should_render_global_menu_item
+
+      menu :project_menu,
+           :resource_management,
+           { controller: "/resource_management/resource_planners", action: :index },
+           caption: :label_resource_management,
+           after: :work_packages,
+           icon: "people"
+    end
+  end
+end
