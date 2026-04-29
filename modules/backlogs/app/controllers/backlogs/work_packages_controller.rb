Kenprof/openproject

move Backlog scope into WorkPackage scope

modules/backlogs/app/controllers/backlogs/work_packages_controller.rb
@@ -112,7 +112,7 @@ module Backlogs
       render_success_flash_message_via_turbo_stream(
         message: I18n.t(:notice_successful_move, from: @sprint.name, to: I18n.t(:label_inbox))
       )
-      work_packages = Backlog.inbox_for(project: @project)
+      work_packages = WorkPackage.backlogs_inbox_for(project: @project)
       replace_via_turbo_stream(
         component: Backlogs::InboxComponent.new(work_packages:, project: @project),
         method: :morph

smithdouglas404/agenticopenproject

modules/backlogs/app/controllers/backlogs/work_packages_controller.rb
@@ -125,7 +125,7 @@ module Backlogs
       render_success_flash_message_via_turbo_stream(
         message: I18n.t(:notice_successful_move, from: @sprint.name, to: I18n.t(:label_inbox))
       )
-      inbox_work_packages = Backlog.inbox_for(project: @project)
+      inbox_work_packages = WorkPackage.backlogs_inbox_for(project: @project)
       backlog_buckets = if OpenProject::FeatureDecisions.backlog_buckets_active?
                           Agile::BacklogBucket.for_project(@project)
                         end