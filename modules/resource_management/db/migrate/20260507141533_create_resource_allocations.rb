a7159630a37025de09ed34d7496b138035f9df80
Klaus Zanders <k.zanders@openproject.com>
2026-05-13 15:00:40 +0200

    Do not allow NULL for the `user_filter` field as this is overwritten by the default

--- modules/resource_management/db/migrate/20260507141533_create_resource_allocations.rb
+++ modules/resource_management/db/migrate/20260507141533_create_resource_allocations.rb
@@ -5,7 +5,7 @@ class CreateResourceAllocations < ActiveRecord::Migration[8.1]
     create_table :resource_allocations do |t|
       t.references :entity, polymorphic: true, null: false
       t.references :principal, foreign_key: { to_table: :users }, null: true
-      t.jsonb :user_filter, default: [], null: true
+      t.jsonb :user_filter, default: []
       t.string :state, null: false
       t.date :start_date, null: false
       t.date :end_date, null: false
