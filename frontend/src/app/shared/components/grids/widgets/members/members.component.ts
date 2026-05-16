c343244aac842f3b36b3d2c72ba5c29b6d216774
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-05-16 12:02:43 +0200

    Add footer for members widget in dashboard

--- frontend/src/app/shared/components/grids/widgets/members/members.component.ts
+++ frontend/src/app/shared/components/grids/widgets/members/members.component.ts
@@ -1,23 +1,29 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { AbstractTurboWidgetComponent } from 'core-app/shared/components/grids/widgets/abstract-turbo-widget.component';
-import { CurrentProjectService } from 'core-app/core/current-project/current-project.service';
 import { CurrentUserService } from 'core-app/core/current-user/current-user.service';
 
 @Component({
   selector: 'op-members-widget',
   templateUrl: './members.component.html',
+  styleUrls: ['./members.component.sass'],
+  encapsulation: ViewEncapsulation.None,
+  host: { class: 'op-widget-members' },
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: false
+  standalone: false,
 })
 export class WidgetMembersComponent extends AbstractTurboWidgetComponent {
-  protected readonly currentProject = inject(CurrentProjectService);
   protected readonly currentUser = inject(CurrentUserService);
 
-  get text() {
-    return { missing_permission: this.i18n.t('js.grid.widgets.missing_permission') };
-  }
+  text = {
+    missing_permission: this.i18n.t('js.grid.widgets.missing_permission'),
+  };
 
-  hasCapability$ = this.currentUser.hasCapabilities$('memberships/read', this.currentProject.id);
+  hasCapability$ = this.currentUser.hasCapabilities$('members/read', this.currentProject.id);
 
   public get projectIdentifier() {
     return this.currentProject.identifier;
