f3d6ec14517bd0401cf0aa8fd330c27c57e43d9b
Alexander Brandon Coles <a.coles@openproject.com>
2026-05-25 13:35:19 +0200

    Fix for Adapter typings

--- frontend/src/turbo/helpers.ts
+++ frontend/src/turbo/helpers.ts
@@ -2,11 +2,11 @@ import * as Turbo from '@hotwired/turbo';
 
 export namespace TurboHelpers {
   export function showProgressBar() {
-    Turbo.session.adapter.formSubmissionStarted();
+    (Turbo.session.adapter as Turbo.BrowserAdapter).formSubmissionStarted({ } as Turbo.FormSubmission);
   }
 
   export function hideProgressBar() {
-    Turbo.session.adapter.formSubmissionFinished();
+    (Turbo.session.adapter as Turbo.BrowserAdapter).formSubmissionFinished({ } as Turbo.FormSubmission);
   }
 
   export function scrubScriptElements(element:HTMLElement|DocumentFragment) {
