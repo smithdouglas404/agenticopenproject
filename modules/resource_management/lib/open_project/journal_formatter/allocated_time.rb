fff35c2ab7214d2be18ee6d23ad91a2829e9bfc5
Klaus Zanders <k.zanders@openproject.com>
2026-06-02 12:37:49 +0200

    Implement journaling for resource allocations

---
+++ modules/resource_management/lib/open_project/journal_formatter/allocated_time.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+# Renders the journaled allocated_time, which is stored in minutes, as a
+# human-readable duration in hours (e.g. "8h"), respecting the instance's
+# duration format setting.
+class OpenProject::JournalFormatter::AllocatedTime < JournalFormatter::Attribute
+  private
+
+  def format_values(values)
+    values.map do |minutes|
+      next if minutes.nil?
+
+      DurationConverter.output(minutes.to_f / 60)
+    end
+  end
+end
