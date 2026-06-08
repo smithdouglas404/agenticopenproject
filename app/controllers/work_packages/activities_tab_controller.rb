63347957ea6b504f647ae3957eab3b9159c526c2
Kabiru Mwenja <k.mwenja@openproject.com>
2026-06-08 19:08:56 +0300

    Extract activities tab polling streams into a service object
    The controller's live-update path spread journal queries, timestamp
    diffing and turbo-stream emission across seven hand-threaded helpers.
    WorkPackages::ActivitiesTab::UpdateStreams now owns the query and
    decision logic and emits through the controller as a stream sink,
    leaving the controller thin and the polling logic unit-testable.

--- app/controllers/work_packages/activities_tab_controller.rb
+++ app/controllers/work_packages/activities_tab_controller.rb
@@ -43,17 +43,7 @@ class WorkPackages::ActivitiesTabController < ApplicationController
   before_action :initialize_pagination, only: %i[index page_streams]
 
   def index
-    render(
-      WorkPackages::ActivitiesTab::LazyIndexComponent.new(
-        work_package: @work_package,
-        journals: @paginated_journals,
-        paginator: @paginator,
-        filter: @filter,
-        last_server_timestamp: get_current_server_timestamp,
-        resolved_anchor: @resolved_anchor
-      ),
-      layout: false
-    )
+    render(lazy_index_shell, layout: false)
   end
 
   def page_streams
@@ -283,12 +273,17 @@ class WorkPackages::ActivitiesTabController < ApplicationController
 
   def perform_update_streams_from_last_update_timestamp
     last_update_timestamp = params[:last_update_timestamp] || params.dig(:journal, :last_update_timestamp)
-    editing_journals = params[:editing_journals]&.split(",")&.map(&:to_i) || []
 
     if last_update_timestamp.present?
-      last_updated_at = Time.zone.parse(last_update_timestamp)
-      generate_time_based_update_streams(last_updated_at, editing_journals)
-      generate_work_package_journals_emoji_reactions_update_streams
+      editing_journals = params[:editing_journals]&.split(",")&.map(&:to_i) || []
+
+      WorkPackages::ActivitiesTab::UpdateStreams.new(
+        work_package: @work_package,
+        filter: @filter,
+        since: Time.zone.parse(last_update_timestamp),
+        editing_journal_ids: editing_journals,
+        sorting: journal_sorting
+      ).emit_into(self)
     else
       @turbo_status = :bad_request
     end
@@ -314,27 +309,31 @@ class WorkPackages::ActivitiesTabController < ApplicationController
 
   def replace_whole_tab
     initialize_pagination # re-initialize pagination to pick up changes to sorting/filtering
-    replace_via_turbo_stream(
-      component: WorkPackages::ActivitiesTab::LazyIndexComponent.new(
-        work_package: @work_package,
-        journals: @paginated_journals,
-        paginator: @paginator,
-        filter: @filter,
-        last_server_timestamp: get_current_server_timestamp,
-        resolved_anchor: @resolved_anchor
-      )
-    )
+    replace_via_turbo_stream(component: lazy_index_shell)
   end
 
   def update_index_component
     initialize_pagination # re-initialize pagination to pick up changes to sorting/filtering
-    update_via_turbo_stream(
-      component: WorkPackages::ActivitiesTab::Journals::LazyIndexComponent.new(
-        work_package: @work_package,
-        journals: @paginated_journals,
-        paginator: @paginator,
-        filter: @filter
-      )
+    update_via_turbo_stream(component: lazy_index_list)
+  end
+
+  def lazy_index_shell
+    WorkPackages::ActivitiesTab::LazyIndexComponent.new(
+      work_package: @work_package,
+      journals: @paginated_journals,
+      paginator: @paginator,
+      filter: @filter,
+      last_server_timestamp: get_current_server_timestamp,
+      resolved_anchor: @resolved_anchor
+    )
+  end
+
+  def lazy_index_list
+    WorkPackages::ActivitiesTab::Journals::LazyIndexComponent.new(
+      work_package: @work_package,
+      journals: @paginated_journals,
+      paginator: @paginator,
+      filter: @filter
     )
   end
 
@@ -359,89 +358,6 @@ class WorkPackages::ActivitiesTabController < ApplicationController
     Journals::UpdateService.new(model: @journal, user: User.current).call(notes:)
   end
 
-  def generate_time_based_update_streams(last_update_timestamp, editing_journals)
-    journals = @work_package
-                 .journals
-                 .internal_visible
-
-    if @filter == Filters::ONLY_COMMENTS
-      journals = journals.where.not(notes: "")
-    end
-
-    grouped_emoji_reactions = EmojiReactions::GroupedQueries.grouped_emoji_reactions_by_reactable(
-      reactable_id: journals.pluck(:id), reactable_type: "Journal"
-    )
-
-    rerender_updated_journals(journals, last_update_timestamp, grouped_emoji_reactions, editing_journals)
-    rerender_journals_with_updated_notification(journals, last_update_timestamp, grouped_emoji_reactions, editing_journals)
-    insert_latest_journals_via_turbo_stream(journals, last_update_timestamp, grouped_emoji_reactions)
-
-    if journals.present?
-      remove_potential_empty_state
-      update_activity_counter
-    end
-  end
-
-  def generate_work_package_journals_emoji_reactions_update_streams
-    @work_package.journals.each do |journal|
-      update_via_turbo_stream(
-        component: WorkPackages::ActivitiesTab::Journals::ItemComponent::Reactions.new(
-          journal:,
-          grouped_emoji_reactions: wp_journals_emoji_reactions[journal.id] || {}
-        )
-      )
-    end
-  end
-
-  def rerender_updated_journals(journals, last_update_timestamp, grouped_emoji_reactions, editing_journals)
-    journals.where("updated_at > ?", last_update_timestamp).find_each do |journal|
-      next if editing_journals.include?(journal.id)
-
-      update_item_show_component(journal:, grouped_emoji_reactions: grouped_emoji_reactions.fetch(journal.id, {}))
-    end
-  end
-
-  def rerender_journals_with_updated_notification(journals, last_update_timestamp, grouped_emoji_reactions, editing_journals)
-    Notification
-      .where(journal_id: journals.pluck(:id))
-      .where(recipient_id: User.current.id)
-      .where("notifications.updated_at > ?", last_update_timestamp)
-      .find_each do |notification|
-        next if editing_journals.include?(notification.journal_id)
-
-        update_item_show_component(
-          journal: journals.find(notification.journal_id),
-          grouped_emoji_reactions: grouped_emoji_reactions.fetch(notification.journal_id, {})
-        )
-      end
-  end
-
-  def insert_latest_journals_via_turbo_stream(journals, last_update_timestamp, emoji_reactions)
-    target_component = WorkPackages::ActivitiesTab::Journals::LazyIndexComponent.new(
-      work_package: @work_package,
-      journals: Journal.none, # we do not need to pass any journals here since we just want the component key
-      paginator: nil,
-      filter: @filter
-    )
-
-    journals.where("created_at > ?", last_update_timestamp).find_each do |journal|
-      insert_via_turbo_stream(
-        target_component:,
-        component: WorkPackages::ActivitiesTab::Journals::ItemComponent.new(
-          journal:, filter: @filter, grouped_emoji_reactions: emoji_reactions.fetch(journal.id, {})
-        ),
-        action: journal_sorting.asc? ? :append : :prepend
-      )
-    end
-  end
-
-  def remove_potential_empty_state
-    # remove the empty state if it is present
-    remove_via_turbo_stream(
-      component: WorkPackages::ActivitiesTab::Journals::EmptyComponent.new
-    )
-  end
-
   def update_item_edit_component(journal:, grouped_emoji_reactions: {})
     update_item_component(journal:, state: :edit, grouped_emoji_reactions:)
   end
@@ -458,14 +374,6 @@ class WorkPackages::ActivitiesTabController < ApplicationController
         filter:,
         grouped_emoji_reactions:
       )
-    )
-  end
-
-  def update_activity_counter
-    # update the activity counter in the primerized tabs
-    # not targeting the legacy tab!
-    replace_via_turbo_stream(
-      component: WorkPackages::Details::UpdateCounterComponent.new(work_package: @work_package, menu_name: "activity")
     )
   end
 
